45c88f9ad6522a08f2d6dd7cddba7758757d18cb
Christer Ostergaard <christer@christer.dk>
2012-09-09 12:31:52 +0200

    Initial commit, with overlay test

---
+++ epay.dk/Controllers/OverlayController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace epay.dk.Controllers
+{
+    public class OverlayController : Controller
+    {
+        //
+        // GET: /Overlay/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult accepturl()
+        {
+            return View("WriteAll");
+        }
+
+        public ActionResult cancelurl()
+        {
+            return View("WriteAll");
+        }
+
+        public ActionResult callbackurl()
+        {
+            return View();
+        }
+
+    }
+}
